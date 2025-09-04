6fc248f91edd8461ace66f098215d154812a58c5
DESKTOP-9STFKGG\keyma <keymasterviriya1150@gmail.com>
2025-09-05 01:13:34 +0700

    feat(app): improve swagger and add hmr
    - Updated Swagger config
    - Added tabWidth to .prettierrc
    - Enabled HMR for dev
    - Improved code formatting
    - Updated validation pipe config

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,32 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('Evacuation example')
-    .setDescription('The evacuation API description')
-    .setVersion('1.0')
-    .addTag('evacuation')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
-  // Enable validation globally
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // Remove properties that don't have decorators
-    forbidNonWhitelisted: true, // Throw error if non-whitelisted properties are provided
-    transform: true, // Automatically transform payloads to DTO instances
-  }));
-  
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder()
+        .setTitle('Evacuation example')
+        .setDescription('The evacuation API description')
+        .setVersion('1.0')
+        .addTag('evacuation')
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, documentFactory);
+    // Enable validation globally
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true, // Remove properties that don't have decorators
+            forbidNonWhitelisted: true, // Throw error if non-whitelisted properties are provided
+            transform: true, // Automatically transform payloads to DTO instances
+        }),
+    );
+
+    await app.listen(process.env.PORT ?? 3000);
+
+    if (module.hot) {
+        module.hot.accept();
+        module.hot.dispose(() => app.close());
+    }
 }
 bootstrap();
